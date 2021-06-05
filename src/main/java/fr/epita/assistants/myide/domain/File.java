50c7b5b2d09ae239282a52d27c658c2e0123564b
Clement BIEBER <clement.bieber@epita.fr>
2021-06-05 18:52:11 +0200

    [IMPLEM] implementation of Node interface for file

---
+++ src/main/java/fr/epita/assistants/myide/domain/File.java
@@ -0,0 +1,33 @@
+package fr.epita.assistants.myide.domain;
+
+import fr.epita.assistants.myide.domain.entity.Node;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+public class File implements Node {
+
+    private final Path path;
+    private final Type type;
+
+    public File(Path path) {
+        this.path = path;
+        this.type = Types.FILE;
+    }
+
+    @Override
+    public Path getPath() {
+        return path;
+    }
+
+    @Override
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public List<Node> getChildren() {
+        return Collections.EMPTY_LIST;
+    }
+}
