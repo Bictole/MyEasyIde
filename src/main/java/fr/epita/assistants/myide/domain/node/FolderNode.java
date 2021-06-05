25df49dcafe40b9dff6803da6263be5ad2432b90
Clement BIEBER <clement.bieber@epita.fr>
2021-06-05 21:37:54 +0200

    [IMPLEM] search function for FolderNode (for further SEARCH feature)

--- src/main/java/fr/epita/assistants/myide/domain/node/FolderNode.java
+++ src/main/java/fr/epita/assistants/myide/domain/node/FolderNode.java
@@ -2,6 +2,7 @@ package fr.epita.assistants.myide.domain.node;
 
 import fr.epita.assistants.myide.domain.entity.Node;
 
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,5 +32,16 @@ public class FolderNode implements Node {
     @Override
     public List<Node> getChildren() {
         return children;
+    }
+
+    public boolean search(String str) throws FileNotFoundException {
+        boolean ret = true;
+        for (var child : children) {
+            if (child.isFolder())
+                ret &= ((FolderNode)child).search(str);
+            else
+                ret &= ((FileNode)child).search(str);
+        }
+        return ret;
     }
 }
