96e0c947ad1a6b5fc531847507f034bd38c2532a
Victor Simonin <victor.simonin@epita.fr>
2021-06-10 15:29:55 +0200

    [Implem] Maven features not tested

--- src/main/java/fr/epita/assistants/ping/feature/maven/Install.java
+++ src/main/java/fr/epita/assistants/ping/feature/maven/Install.java
@@ -3,11 +3,52 @@ package fr.epita.assistants.ping.feature.maven;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.apache.maven.DefaultMaven;
+import org.apache.maven.Maven;
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequest;
+
+import java.util.Arrays;
 
 public class Install implements Feature {
+    private class ExecutionReportInstall implements Feature.ExecutionReport {
+        public final boolean success;
+        public String errorMessage = "";
+
+        public ExecutionReportInstall() {
+            this.success = true;
+        }
+
+        public ExecutionReportInstall(String errorMessage) {
+            this.success = false;
+            this.errorMessage = errorMessage;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
     @Override
     public Feature.ExecutionReport execute(Project project, Object... params) {
-        return null;
+
+        try {
+            Maven mvn = new DefaultMaven();
+            MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+            request.setBaseDirectory(project.getRootNode().getPath().toFile());
+            request.setGoals(Arrays.asList("install"));
+            mvn.execute(request);
+            return new Install.ExecutionReportInstall();
+        }
+        catch (Exception e)
+        {
+            return new Install.ExecutionReportInstall("Maven Install failed :" + e.getMessage());
+        }
     }
 
     @Override
