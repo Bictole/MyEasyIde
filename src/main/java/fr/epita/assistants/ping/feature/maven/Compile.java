a06ea7351bd86cadae116973f6f692fad9dcec64
Jeanne MORIN <jeanne.morin@epita.fr>
2021-06-10 15:43:17 +0200

    [IMPLEM] Maven feature

--- src/main/java/fr/epita/assistants/ping/feature/maven/Compile.java
+++ src/main/java/fr/epita/assistants/ping/feature/maven/Compile.java
@@ -3,12 +3,52 @@ package fr.epita.assistants.ping.feature.maven;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.apache.maven.DefaultMaven;
+import org.apache.maven.Maven;
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequest;
+
+import java.util.Arrays;
 
 public class Compile implements Feature {
+    private class ExecutionReportCompile implements Feature.ExecutionReport {
+        public final boolean success;
+        public String errorMessage = "";
+
+        public ExecutionReportCompile() {
+            this.success = true;
+        }
+
+        public ExecutionReportCompile(String errorMessage) {
+            this.success = false;
+            this.errorMessage = errorMessage;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
+
     @Override
     public Feature.ExecutionReport execute(Project project, Object... params) {
-
-        return null;
+        try {
+            Maven mvn = new DefaultMaven();
+            MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+            request.setBaseDirectory(project.getRootNode().getPath().toFile());
+            request.setGoals(Arrays.asList("compile"));
+            mvn.execute(request);
+            return new Compile.ExecutionReportCompile();
+        }
+        catch (Exception e)
+        {
+            return new Compile.ExecutionReportCompile("Maven Compile failed :" + e.getMessage());
+        }
     }
 
     @Override
--- src/main/java/fr/epita/assistants/ping/feature/maven/Test.java
+++ src/main/java/fr/epita/assistants/ping/feature/maven/Test.java
@@ -3,11 +3,50 @@ package fr.epita.assistants.ping.feature.maven;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.apache.maven.DefaultMaven;
+import org.apache.maven.Maven;
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequest;
+
+import java.util.Arrays;
 
 public class Test implements Feature {
+
+    private class ExecutionReportTest implements Feature.ExecutionReport {
+        public final boolean success;
+        public String errorMessage = "";
+
+        public ExecutionReportTest() {
+            this.success = true;
+        }
+
+        public ExecutionReportTest(String errorMessage) {
+            this.success = false;
+            this.errorMessage = errorMessage;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
     @Override
     public Feature.ExecutionReport execute(Project project, Object... params) {
-        return null;
+        try {
+            Maven mvn = new DefaultMaven();
+            MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+            request.setBaseDirectory(project.getRootNode().getPath().toFile());
+            request.setGoals(Arrays.asList("test"));
+            mvn.execute(request);
+            return new Test.ExecutionReportTest();
+        } catch (Exception e) {
+            return new Test.ExecutionReportTest("Maven Test failed :" + e.getMessage());
+        }
     }
 
     @Override
