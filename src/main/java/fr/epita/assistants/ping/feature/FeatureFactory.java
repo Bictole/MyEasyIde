816a48b74be1049c1423751d3f2043cdd0bf51c1
Clement BIEBER <clement.bieber@epita.fr>
2021-06-18 00:11:05 +0200

    [IMPLEM] finished FeatureFactory design for getFeature

--- src/main/java/fr/epita/assistants/ping/feature/FeatureFactory.java
+++ src/main/java/fr/epita/assistants/ping/feature/FeatureFactory.java
@@ -4,6 +4,13 @@ import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.ping.feature.any.CleanUp;
 import fr.epita.assistants.ping.feature.any.Dist;
+import fr.epita.assistants.ping.feature.any.Search;
+import fr.epita.assistants.ping.feature.git.Add;
+import fr.epita.assistants.ping.feature.git.Commit;
+import fr.epita.assistants.ping.feature.git.Pull;
+import fr.epita.assistants.ping.feature.git.Push;
+import fr.epita.assistants.ping.feature.maven.*;
+import fr.epita.assistants.ping.feature.maven.Package;
 
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -14,24 +21,33 @@ public class FeatureFactory {
     private enum Types {
 
         // ANY FEATURES
-        DIST(Mandatory.Features.Any.DIST),
-        CLEANUP(Mandatory.Features.Any.CLEANUP),
-        SEARCH(Mandatory.Features.Any.SEARCH),
+        DIST(Mandatory.Features.Any.DIST, new Dist()),
+        CLEANUP(Mandatory.Features.Any.CLEANUP, new CleanUp()),
+        SEARCH(Mandatory.Features.Any.SEARCH, new Search()),
 
         // GIT FEATURES
-        ADD(Mandatory.Features.Git.ADD),
-        COMMIT(Mandatory.Features.Git.COMMIT),
-        PULL(Mandatory.Features.Git.PULL),
-        PUSH(Mandatory.Features.Git.PUSH),
+        ADD(Mandatory.Features.Git.ADD, new Add()),
+        COMMIT(Mandatory.Features.Git.COMMIT, new Commit()),
+        PULL(Mandatory.Features.Git.PULL, new Pull()),
+        PUSH(Mandatory.Features.Git.PUSH, new Push()),
 
         // MAVEN FEATURES
-        CLEAN(Mandatory.Features.Maven.CLEAN);
+        CLEAN(Mandatory.Features.Maven.CLEAN, new Clean()),
+        COMPILE(Mandatory.Features.Maven.COMPILE, new Compile()),
+        EXEC(Mandatory.Features.Maven.EXEC, new Exec()),
+        INSTALL(Mandatory.Features.Maven.INSTALL, new Install()),
+        PACKAGE(Mandatory.Features.Maven.PACKAGE, new Package()),
+        TEST(Mandatory.Features.Maven.TEST, new Test()),
+        TREE(Mandatory.Features.Maven.TREE, new Tree());
 
         public final Feature.Type type;
+        public final Feature feature;
 
-        Types(Feature.Type type) {
+        Types(Feature.Type type, Feature feature) {
             this.type = type;
+            this.feature = feature;
         }
+
         private static final Map<Feature.Type, Types> BY_TYPE = new HashMap<>();
 
         static {
@@ -55,18 +71,8 @@ public class FeatureFactory {
     }
 
     private static Feature addFeature(Feature.Type type) {
-        Feature feature;
         Types enumType = Types.typeOfType(type);
-        switch (enumType) {
-            case DIST:
-                feature = new Dist();
-                break;
-            case CLEANUP:
-                feature = new CleanUp();
-                break;
-            default:
-                feature = null;
-        }
+        Feature feature = enumType.feature;
         if (feature == null)
             throw new IllegalArgumentException("Given type is not valid");
         featureTable.put(type, feature);
